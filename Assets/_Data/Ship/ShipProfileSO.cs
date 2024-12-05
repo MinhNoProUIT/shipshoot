98363c80e551b9872518557ee38abeb254103b1b
MinhNoProUIT <154415039+MinhNoProUIT@users.noreply.github.com>
2024-12-06 03:45:43 +0700

    Update Shop Ship

---
+++ Assets/_Data/Ship/ShipProfileSO.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "ShipProfileSO", menuName = "SO/ShipProfile")]
+
+public class ShipProfileSO : ScriptableObject
+{
+    public IdShip ShipId = IdShip.NoId;
+    public string ShipName;
+    public Sprite sprite;
+    public int dameMax;
+    public int hpMax;
+    public int levelUnlock;
+    public int coins;
+    public int diamonds;
+}
+
+public enum IdShip
+{
+    NoId = 0,
+
+    SH001 = 1,
+
+    SH002 = 2,
+    SH003 = 3,
+
+    SH004 = 4,
+    SH005 = 5,
+    SH006 = 6,
+    SH007 = 7,
+    SH008 = 8,
+    SH009 = 9,
+    SH010 = 10,
+}
